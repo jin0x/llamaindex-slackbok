90b5f7169235b1064e38fa7c4d7a3bd9eb54781b
John Leskas <john.leskas@gmail.com>
2024-04-27 02:34:56 +0300

    feat: first poc of creating a wc request

--- 9_knowledge_base_rag_in_slack.py
+++ 9_knowledge_base_rag_in_slack.py
@@ -2,7 +2,12 @@
 import dotenv, os
 dotenv.load_dotenv()
 
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+import re
+import json
+import requests
 from pyboxen import boxen
 
 # Bring in deps including Slack Bolt framework
@@ -15,9 +20,8 @@ from llama_index.core import (
     VectorStoreIndex,
     Document,
     Settings,
-    SimpleDirectoryReader,
+    # SimpleDirectoryReader,
     StorageContext,
-    SummaryIndex,
     load_index_from_storage,
 )
 from llama_index.llms.ollama import Ollama
@@ -36,7 +40,7 @@ if not os.path.exists(PERSIST_DIR):
     # load the documents and create the index
     # documents = SimpleDirectoryReader('data').load_data()
     documents = SimpleWebPageReader(html_to_text=True).load_data(
-        ['https://woocommerce.github.io/woocommerce-rest-api-docs/?shell#introduction']
+        ['http://woocommerce.github.io/woocommerce-rest-api-docs/?shell#introduction']
     )
     index = VectorStoreIndex.from_documents(documents, show_progress=True, transformations=[text_splitter])
     index.storage_context.persist()
@@ -48,8 +52,10 @@ else:
 SYSTEM_PROMPT = (
     "As a WooCommerce AI Store Assistant, your role is to process requests related to store management "
     "tasks such as generating coupons or creating new products. For each user request, provide a response "
-    "that includes the corresponding cURL command for the requested action. If a request cannot be fulfilled "
-    "because it is not supported by your current capabilities, instruct the user to modify their inquiry. "
+    "that includes the corresponding payload that needed in an object format and the full endpoint URL without the domain URL, only everything after and including /wp-json/wc.",
+    "Your response should look exactly like this <PAYLOAD>Here you should put the payload object</PAYLOAD><ENDPOINT>Here you should put the endpoint</ENDPOINT>",
+    "You should only include the payload and the endpoint inside the template tags",
+    "If a request cannot be fulfilled because it is not supported by your current capabilities, instruct the user to modify their inquiry."
     "Always ensure your responses are actionable and precise."
 )
 
@@ -76,6 +82,62 @@ bot_user_id = auth_response["user_id"]
 
 def boxen_print(*args, **kwargs):
     print(boxen(*args, **kwargs))
+
+
+def dispatch_store_action(data):
+    payload_match = re.search(r'<PAYLOAD>(.*?)</PAYLOAD>', data, re.DOTALL)
+    endpoint_match = re.search(r'<ENDPOINT>(.*?)</ENDPOINT>', data, re.DOTALL)
+
+    print(type(data))
+    print(f"Payload Match: {payload_match}")
+    print(f"Endpoint Match: {endpoint_match}")
+
+
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    if payload_match and endpoint_match:
+        payload = payload_match.group(1).strip()
+        endpoint = endpoint_match.group(1).strip()
+        full_url = f"{os.environ.get("WC_DOMAIN")}/{endpoint}"
+
+        boxen_print(
+            f"Payload: {payload}",
+            title="Payload",
+            color="yellow"
+        )
+        boxen_print(
+            f"Payload: {endpoint}",
+            title="Endpoint",
+            color="yellow"
+        )
+
+        headers = {'Content-Type': 'application/json'}
+
+        response = requests.post(
+            full_url,
+            headers=headers,
+            json=json.loads(payload),
+            auth=(os.environ.get("WC_CONSUMER_KEY"), os.environ.get("WC_CONSUMER_SECRET")),
+            verify=False
+        )
+
+        if response.status_code == 200:
+            response_text = f"API call successful!"
+        else:
+            response_text = f"Failed API call: {response.status_code} - {response.text}"
+
+        # Response box
+        boxen_print(
+            response_text,
+            title="API Response",
+            color="green" if response.status_code == 200 or response.status_code == 201  else "red"
+        )
+
+    else:
+        boxen_print("Failed to extract payload or endpoint.", title="Error", color="red")
+
 
 # this is the challenge route required by Slack
 # if it's not the challenge it's something for Bolt to handle
@@ -131,12 +193,14 @@ def reply(message, say):
                                     )
                                     # Response box
                                     boxen_print(
-                                        f"Response was: {response}",
+                                        f"Response was: {str(response)}",
                                         title="AI Bot",
                                         color="green"
                                     )
 
-                                    say(str(response))
+                                    dispatch_store_action(str(response))
+                                    say("Operation completed")
+                                    # say(str(response))
                                     return
     # otherwise treat it as a document to store
     index.insert(Document(text=message.get('text')))
